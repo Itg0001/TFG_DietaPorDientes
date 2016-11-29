1c53d7801d35e17526961f0b7322ed905ade5dde
Itg0001 <itg0001@alu.ubu.es>
2016-11-29 16:18:16 +0100

    #57 Semana 11: Crear ayuda para la aplicación.
    Esta tarea ha consistido en investigar sobre QtHelp que es la clase que
    implementaría la ayuda para aplicaciones en python pero no aparece aun
    en la propia documentación ninguna explicación sobre ello investigando
    hemos descubierto que está disponible para QT pero aun no para PyQt. Por
    lo que se nos ha ocurrido hacer un visor de documentos html para pasar
    la futura ayuda de látex a un documento html y así visualizarlo desde
    aquí.

--- Ejecutar_Test.bat
+++ Ejecutar_Test.bat
@@ -1,7 +1,9 @@
-IF NOT EXIST %HOMEPATH%\Miniconda3 Miniconda3-latest-Windows-x86_64.exe
-IF NOT EXIST %HOMEPATH%\DietaEstrias conda env create -f environment.yml
-set path=%HOMEPATH%\DietaEstrias;%path%;
+REM IF NOT EXIST %HOMEPATH%\Miniconda3 Miniconda3-latest-Windows-x86_64.exe
 
+IF NOT EXIST %HOMEPATH%\Miniconda3\envs\DietaEstrias conda env create -f %cd%\environment.yml
+
+
+set path=%HOMEPATH%\Miniconda3\envs\DietaEstrias; %path%;
 cd src
 python mainTest.py
 

a3145fd13f18f0bb4734c3905e6574ae0bd14257
Itg0001 <itg0001@alu.ubu.es>
2016-12-27 19:25:31 +0100

    #75 Semana 15: Documentar.
    Este commit se corresponde con parte de la corrección de la
    documentación por mi parte  con las recomendaciones de los tutores.
    También hemos añadido el cálculo del punto Que forma la dieta con esos
    estadísticos.

--- src/proyecto/analisis/informes/DatosToCsv.py
+++ src/proyecto/analisis/informes/DatosToCsv.py
@@ -47,8 +47,24 @@ class DatosToCsv(InGuardarDatos):
             salida.writerows(lista[3])
         del salida
         csvsalida_lin.close()
-            
-            
+
+        desv_v=float(lista[4][3])
+        num_h=float(lista[5][1])
+        med_h=float(lista[5][2])
+        desv_h=float(lista[5][3])
+        num_md=float(lista[6][1])
+        desv_dm=float(lista[7][3])
+        num_tot=float(lista[8][1])
+        med_tot=float(lista[8][2])
+        desv_tot=float(lista[8][3])
+
+        f1=round((0.11364*num_h)-(0.03017*num_md)-(0.00169*desv_dm)-(0.01485*med_tot)+(0.00958*desv_tot)-(-0.00468*desv_v)+(0.00454*med_h)+(0.00203*num_tot)+0.06819,3)
+        f2=round((-0.05361*num_h)+(0.09573*num_md)+(0.00730*desv_h)-(0.01340*med_tot)-(0.00922*desv_tot)+(0.01098*desv_v)+(0.00061*med_h)+(0.03253*num_tot)-0.42014,3)
+        pt1="punto_x"
+        pt2="punto_y"
+        punto=[]
+        punto.extend([pt1,f1,pt2,f2])
+        
         csvsalida_stat = open(path.replace('\\', '/') + self.dic.sal_estad, 'w', newline='')
         salida = csv.writer(csvsalida_stat, escapechar=' ', quoting=csv.QUOTE_NONE, delimiter=';')
         salida.writerow(self.dic.cabecera_csv_estad)
@@ -57,6 +73,9 @@ class DatosToCsv(InGuardarDatos):
         salida.writerow(lista[6])
         salida.writerow(lista[7])
         salida.writerow(lista[8])
+        salida.writerow(punto)
+
+ 
         del salida
         csvsalida_stat.close()
         
